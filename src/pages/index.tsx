2478ed81582b5e6fe406356944d5522fdd49d601
jibijohndavid <jibijohndavid@gmail.com>
2022-08-18 01:05:06 +0530

    Added prettier and updated eslint configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>Next Tailwind Starter</title>
-        <meta name="description" content="Next Tailwind Starter" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Next Tailwind Starter' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className="flex justify-center items-center h-screen">
-        <h1 className="text-3xl font-bold text-gray-800">
+      <main className='flex h-screen items-center justify-center'>
+        <h1 className='text-3xl font-bold text-gray-800'>
           Next Tailwind Starter
         </h1>
       </main>
