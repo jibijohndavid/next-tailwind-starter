0c410353a3298fd2c1eeeed75c3b05bdf1bb5405
jibijohndavid <jibijohndavid@gmail.com>
2022-08-17 23:46:12 +0530

    🚀 Init

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>🚀 next-tailwind-starter</h1>
+<p>Next.js + Tailwind CSS + TypeScript starter packed with useful development features.</p>
+<p>Made by <a href="https://jibi.dev">Jibi John David</a></p>
+
+## Features
+
+This repository is packed with:
+
+- ⚡️ Next.js 12
+- ⚛️ React 18
+- ✨ TypeScript
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Next Tailwind Starter</title>
+        <meta name="description" content="Next Tailwind Starter" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Next Tailwind Starter</h1>
+        <p>
+          Get started by editing <code>pages/index.tsx</code>
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
